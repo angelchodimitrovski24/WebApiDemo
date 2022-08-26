ef4f3b5bc59b58a1f0467ef89171528053b5eaf2
Angelcho Dimitrovski <angelchodimitrovski24@gmail.com>
2022-08-26 20:24:37 +0200

    Added comment documentation file App_Data\Documentation.xml and publisched in folder

--- DemoApi/Controllers/PeopleController.cs
+++ DemoApi/Controllers/PeopleController.cs
@@ -12,12 +12,21 @@ namespace DemoApi.Controllers
     {
         List<Person> people = new List<Person>();
 
+        /// <summary>
+        /// All the information about peoplecontroller example
+        /// </summary>
         public PeopleController()
         {
             people.Add(new Person { FirstName = "Gabriel", LastName = "Dimitrovski", Id = 1});
             people.Add(new Person { FirstName = "Angelcho", LastName = "Dimitrovski", Id = 2 });
             people.Add(new Person { FirstName = "Hristina", LastName = "Dimitrovska", Id = 3 });
         }
+        /// <summary>
+        /// Get a first names of all users
+        /// </summary>
+        /// <param name="userId">Unique identifier for this person</param>
+        /// <param name="age"><Age of the person/param>
+        /// <returns>Return a list of first names</returns>
         [Route("api/People/GetFirstNames/{userId:int}/{age:int}")]
         [HttpGet]
         public List<string> GetFirstNames(int userId, int age)
--- DemoApi/Models/Person.cs
+++ DemoApi/Models/Person.cs
@@ -5,10 +5,24 @@ using System.Web;
 
 namespace DemoApi.Models
 {
+    /// <summary>
+    /// Represents one specific person
+    /// </summary>
     public class Person
     {
+        /// <summary>
+        /// Id from SQL
+        /// </summary>
         public int Id { get; set; } = 0;
+
+        /// <summary>
+        /// User's first name
+        /// </summary>
         public string FirstName { get; set; } = "";
+
+        /// <summary>
+        /// User's last name
+        /// </summary>
         public string LastName { get; set; } = "";
 
     }
